b5ef09c818b7f85a7ee5b15ededed9bdf6b00815
zaqwsx55 <zaqwsx@poczta.fm>
2017-11-15 13:23:35 +0100

    Serwis WorkerService

--- src/app/worker.service.ts
+++ src/app/worker.service.ts
@@ -3,6 +3,33 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class WorkerService {
 
+  private workers = [
+    {
+      firstName: 'Marcin',
+      lastName: 'R',
+      gender: 'm'
+    },
+    {
+      firstName: 'Piotr',
+      lastName: 'B',
+      gender: 'm'
+    },
+    {
+      firstName: 'Marzena',
+      lastName: 'Ł',
+      gender: 'f'
+    },
+    {
+      firstName: 'Czesław',
+      lastName: 'O',
+      gender: 'm'
+    }
+  ];
+
   constructor() { }
+
+  getWorkers() {
+    return this.workers;
+  }
 
 }
