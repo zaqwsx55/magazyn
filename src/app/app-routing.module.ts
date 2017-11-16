b9a5ab33e0c98379b1c91208bf9d4cb032e6bac4
zaqwsx55 <zaqwsx@poczta.fm>
2017-11-16 14:42:35 +0100

    Wprowadzone nowe ścieżki

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,24 @@ import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { WorkersComponent } from './workers/workers.component';
+import { WorkerDetailComponent } from './worker-detail/worker-detail.component';
+import { WorkerEditComponent } from './worker-edit/worker-edit.component';
 import { DevicesComponent } from './devices/devices.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
+/**
+ * TODO
+ * te ścieżki są do poprawy
+ * powinno być przez ChildComponent
+ */
 const routes = [
   { path: 'workers', component: WorkersComponent },
-  { path: 'devices', component: DevicesComponent }
+  { path: 'workers/:id', component: WorkerDetailComponent },
+  { path: 'workeredit', component: WorkerEditComponent },
+  { path: 'workeredit/:id', component: WorkerEditComponent },
+  { path: 'devices', component: DevicesComponent },
+  { path: '', redirectTo: '/workers', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent }
 ]
 
 @NgModule({
