3427ed098af0de29dfaa902fd1a60db77d40e578
zaqwsx55 <zaqwsx@poczta.fm>
2017-11-16 14:39:34 +0100

    Nowy komponent: WorkerEdit

---
+++ src/app/worker-edit/worker-edit.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-worker-edit',
+  templateUrl: './worker-edit.component.html',
+  styleUrls: ['./worker-edit.component.css']
+})
+export class WorkerEditComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
