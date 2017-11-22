e55fb644620a906c0c64ae73c706ea2354fe914c
zaqwsx55 <zaqwsx@poczta.fm>
2017-11-22 21:34:45 +0100

    Zmiana formularza na Responsive Form

--- src/app/worker-edit/worker-edit.component.ts
+++ src/app/worker-edit/worker-edit.component.ts
@@ -1,15 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-
-
-
-import { FormControl } from '@angular/forms';
-
-
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 import { Observable } from 'rxjs/Observable';
 
-import { WorkerService } from './../worker.service';
+import { WorkerService } from '../worker.service';
 import { Worker } from '../worker';
 
 @Component({
@@ -19,30 +14,39 @@ import { Worker } from '../worker';
 })
 export class WorkerEditComponent implements OnInit {
 
-  firstName = new FormControl();
-  lastName = new FormControl();
-  gender = new FormControl();
+  workerForm: FormGroup;
 
   workerId: string;
   worker: Observable<any>;
 
-  constructor(private activatedRoute: ActivatedRoute, private workerService: WorkerService) { }
+  constructor(private activatedRoute: ActivatedRoute,
+              private workerService: WorkerService,
+              private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.createForm();
     this.workerId = this.activatedRoute.snapshot.params.id;
     this.getWorker();
+  }
+
+  createForm() {
+    this.workerForm = this.formBuilder.group({
+      firstName: ['', Validators.required],
+      lastName: ['', [Validators.required, Validators.maxLength(1)]],
+      gender: ''
+    });
   }
 
   getWorker() {
     if (this.workerId) {
       this.worker = this.workerService.getWorker(this.workerId);
       this.worker.subscribe((w) => {
-        this.firstName.setValue(w.firstName);
-        this.lastName.setValue(w.lastName);
-        this.gender.setValue(w.gender);
+        this.workerForm.setValue({
+          firstName: w.firstName,
+          lastName: w.lastName,
+          gender: w.gender
+        });
       });
-    } else {
-      // this.name.setValue('');
     }
   }
 
