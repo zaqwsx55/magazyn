4ddc735c01c1edd39835c5df751386c71dfdbf65
zaqwsx55 <zaqwsx@poczta.fm>
2017-11-24 14:49:36 +0100

    Usunięcie niepotrzebnej spacji

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
 
-import { ReactiveFormsModule } from '@angular/forms'; 
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 import { HttpClientModule } from '@angular/common/http';
