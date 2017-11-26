6ffe661f80ca1dca61f05ca3120c996fd605289b
zaqwsx55 <zaqwsx@poczta.fm>
2017-11-26 22:18:16 +0100

    Zmiany w komponencie WorkerEditComponent

--- src/app/worker-detail/worker-detail.component.ts
+++ src/app/worker-detail/worker-detail.component.ts
@@ -1,6 +1,9 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Component, OnInit, Inject } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
 import { Observable } from 'rxjs/Observable';
+
+import { MatDialog, MatDialogRef } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 import { Worker } from '../worker';
 import { WorkerService } from './../worker.service';
@@ -22,8 +25,13 @@ export class WorkerDetailComponent implements OnInit {
 
   private workerId: string;
   worker: Observable<any>;
+  showSpinner = true;
 
-  constructor(private route: ActivatedRoute, private workerService: WorkerService) { }
+  constructor(private route: ActivatedRoute,
+              private router: Router,
+              private workerService: WorkerService,
+              private dialog: MatDialog,
+              private snackBar: MatSnackBar) { }
 
   ngOnInit() {
     this.workerId = this.route.snapshot.params.id;
@@ -32,6 +40,46 @@ export class WorkerDetailComponent implements OnInit {
 
   getWorker() {
     this.worker = this.workerService.getWorker(this.workerId);
+    this.worker.subscribe(() => this.showSpinner = false);
+  }
+
+  openDialog(): void {
+    const dialogRef = this.dialog.open(DialogConfirmComponent);
+    dialogRef.afterClosed().subscribe(result => {
+      if (result === 'Confirm') {
+        console.log('potwierdzone');
+        this.workerService.deleteWorker(this.workerId);
+        this.snackBar.open('Usunięto pracownika', 'OK', {
+          duration: 3000
+        });
+        this.router.navigate(['/workers']);
+      }
+    })
+  }
+
+  confirmed() {
+    console.log('inside confirmed');
+  }
+
+
+}
+
+@Component({
+  selector: 'app-dialog-confirm',
+  templateUrl: 'dialog-confirm.html'
+})
+export class DialogConfirmComponent {
+
+  constructor(private dialogRef: MatDialogRef<DialogConfirmComponent>) { }
+
+  onCloseConfirm() {
+    console.log('inside onCloseConfirm');
+    this.dialogRef.close('Confirm');
+  }
+
+  onCloseCancel() {
+    console.log('inside onCloseCancel');
+    this.dialogRef.close('Cancel');
   }
 
 }
