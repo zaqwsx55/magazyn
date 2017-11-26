0ad9e27a6c15434731536692f7e8bc6a731bf0c6
zaqwsx55 <zaqwsx@poczta.fm>
2017-11-26 22:16:22 +0100

    Zmiany w komponencie WorkersComponent

--- src/app/workers/workers.component.ts
+++ src/app/workers/workers.component.ts
@@ -1,8 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormControl } from '@angular/forms';
 import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/debounceTime';
 
 import { WorkerService } from '../worker.service';
 import { Worker } from '../worker';
+import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
+
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-workers',
@@ -13,16 +18,67 @@ export class WorkersComponent implements OnInit {
 
   workers: Observable<any[]>;
   showSpinner = true;
+  sortForm: FormGroup;
+  sortType = 'firstName-asc';
 
-  constructor(private workersService: WorkerService) { }
+  workersArray = [];
+  workersFilteredArray = [];
+  // filteredWorkers = [];
+
+
+  constructor(private workersService: WorkerService,
+              private formBuilder: FormBuilder,
+              private router: Router) { }
 
   ngOnInit() {
     this.getWorkers();
+    this.createForm();
+
+
+    this.sortForm.get('search').valueChanges
+      .debounceTime(300)
+      .subscribe(term => {
+        const filterBy = term ? term.toLowerCase() : null;
+        const filteredWorkers = filterBy ? this.workersArray.filter(
+          worker => worker.firstName.toLowerCase().indexOf(filterBy) !== -1) : this.workersArray;
+        this.workersFilteredArray = filteredWorkers;
+      });
+
   }
 
+  createForm() {
+    this.sortForm = this.formBuilder.group({
+      search: [''],
+      sort: [this.sortType],
+      department: ['']
+    });
+  }
+
+  displayFn(worker: Worker) {
+    console.log('inside displayFn');
+    return worker ? worker.firstName + ' ' + worker.lastName : worker;
+  }
+
+  selectWorker(event: MatAutocompleteSelectedEvent) {
+    const worker: Worker = event.option.value;
+    this.router.navigate(['workers', worker.id]);
+  }
+
+  onSortChanged() {
+    this.sortType = this.sortForm.get('sort').value;
+    this.getWorkers();
+  }
+
+
   getWorkers() {
-    this.workers = this.workersService.getWorkers();
-    this.workers.subscribe(() => this.showSpinner = false);
+    const sortField = this.sortType.split('-')[0];
+    const sortOrder = this.sortType.split('-')[1];
+    this.workers = this.workersService.getWorkers(sortField, sortOrder);
+    this.workers.subscribe((workers) => {
+      this.workersArray = workers;
+      this.workersFilteredArray = workers;
+      this.showSpinner = false
+    });
   }
 
 }
