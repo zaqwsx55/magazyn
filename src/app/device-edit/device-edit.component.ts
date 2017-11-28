31d0bcfab962656ba1073869c98b1441f1ac470c
zaqwsx55 <zaqwsx@poczta.fm>
2017-11-28 14:54:46 +0100

    Napisany od nowa komponent DeviceEdit

--- src/app/device-edit/device-edit.component.ts
+++ src/app/device-edit/device-edit.component.ts
@@ -1,4 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnChanges } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Observable } from 'rxjs/Observable';
+import { Device } from '../device';
+import { DeviceService } from '../device.service';
+import { DeviceType } from '../device-type';
+
+import { WorkerService } from '../worker.service';
+import { MatSelectChange } from '@angular/material/select';
 
 @Component({
   selector: 'app-device-edit',
@@ -7,9 +16,89 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DeviceEditComponent implements OnInit {
 
-  constructor() { }
+  pageTitle = 'Dodaj nowy sprzęt';
+  deviceForm: FormGroup;
+  deviceId: string;
+  device: Observable<any>;
+  deviceTypes: Observable<any[]>;
+
+  workers: Observable<any[]>;
+
+
+  selectedType = '';
+
+
+
+  testObj = [{
+    name: 'Laptop',
+    icon: 'laptop'
+  }, {
+    name: 'Drukarka',
+    icon: 'print'
+  }];
+
+  constructor(private router: Router,
+              private activatedRoute: ActivatedRoute,
+              private formBuilder: FormBuilder,
+              private deviceService: DeviceService,
+              private workerService: WorkerService) { }
 
   ngOnInit() {
+    this.createForm();
+
+    this.deviceTypes = this.deviceService.getDeviceTypes();
+    console.log(this.deviceTypes);
+
+    this.workers = this.workerService.getWorkers('firstName', 'asc');
+
+    this.deviceId = this.activatedRoute.snapshot.params.id;
+    if (this.deviceId) {
+      this.pageTitle = 'Edytuj dane sprzętu';
+      this.getDevice();
+    }
   }
+
+  createForm() {
+    this.deviceForm = this.formBuilder.group({
+      name: ['', Validators.required],
+      type: this.formBuilder.group({
+        name: '',
+        icon: ''
+      }),
+      worker: ['']
+    });
+  }
+
+  getDevice() {
+    if (this.deviceId) {
+      this.device = this.deviceService.getDevice(this.deviceId);
+      this.device.subscribe((d) => {
+        this.deviceForm.setValue({
+          name: d.name,
+        });
+      });
+    }
+  }
+
+  saveDevice() {
+    let device = new Device();
+    device = this.deviceForm.value;
+
+    console.log(device);
+
+
+    if (this.deviceId) {
+      this.deviceService.updateDevice(this.deviceId, this.deviceForm.value)
+    } else {
+      console.log('save device: ' + this.deviceForm.value);
+      this.deviceService.addDevice(this.deviceForm.value);
+    }
+    this.goToDevices();
+  }
+
+  goToDevices() {
+    this.router.navigate(['devices']);
+  }
+
 
 }
