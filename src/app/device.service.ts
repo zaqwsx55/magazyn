5027b387eb0a0275cfb0cda0983061e87b59edb8
zaqwsx55 <zaqwsx@poczta.fm>
2017-11-28 14:54:22 +0100

    Nowe metody

--- src/app/device.service.ts
+++ src/app/device.service.ts
@@ -4,6 +4,7 @@ import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 
 import { Device } from './device';
+import { DeviceType } from './device-type';
 
 @Injectable()
 export class DeviceService {
@@ -14,6 +15,8 @@ export class DeviceService {
   deviceDocument: AngularFirestoreDocument<Device>;
   device: Observable<Device>;
 
+  deviceTypes: Observable<any[]>;
+
   constructor(private db: AngularFirestore) { }
 
   getDevices() {
@@ -21,6 +24,9 @@ export class DeviceService {
     this.devices = this.deviceCollection.snapshotChanges().map(actions => {
       return actions.map(a => {
         const data = a.payload.doc.data() as Device;
+
+
+
         const id = a.payload.doc.id;
         return {id, ...data};
       });
@@ -32,6 +38,25 @@ export class DeviceService {
     this.deviceDocument = this.db.doc<Device>('/devices/' + id);
     this.device = this.deviceDocument.valueChanges();
     return this.device;
+  }
+
+  addDevice(device: Device) {
+    this.db.collection('devices').add(device);
+    console.log('New Device added: ' + device);
+  }
+
+  updateDevice(id: string, newDevice: Device) {
+    this.db.doc('devices/' + id).update(newDevice);
+    console.log('Device with id: ' + id + ' was updated');
+  }
+
+  deleteDevice(id: string) {
+    this.db.doc('devices/' + id).delete();
+  }
+
+  getDeviceTypes() {
+    this.deviceTypes = this.db.collection('device-type').valueChanges();
+    return this.deviceTypes;
   }
 
 }
